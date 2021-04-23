8c98776360e29bd88303728846cae9ba0cf34c5d
ait <ait@ex.in>
2021-04-24 02:27:39 +0900

    Add CSS Module

---
+++ src/components/ModuleCss.jsx
@@ -0,0 +1,13 @@
+import css from './ModuleCss.module.scss'
+
+function ModuleCss() {
+    return (
+        <div>
+            <p className={css.title}>
+                ModuleCss
+            </p>
+        </div>
+    )
+}
+
+export default ModuleCss
