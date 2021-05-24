d48a0b4837a2ab59dbe58095eb3d5f63140db3d2
ait <ait@ex.in>
2021-05-24 10:43:27 +0900

    to Memo for Other Props Al

--- src/pages/Page1_Child.jsx
+++ src/pages/Page1_Child.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { memo } from 'react'
 
 const style = {
     background: 'khaki',
     color: 'orange',
 }
 
-export const Page1_Child = (props) => {
+export const Page1_Child = memo((props) => {
     const { isOpen } = props
+    console.log('レンダリング Page1_Child')
+
+    const data = [...Array(200).keys()]
+    console.log(data)
+
+    data.forEach(() => {
+        console.log('xxx...')
+    })
     return <div>{isOpen ? <div style={style}>Open!!</div> : <div>null</div>}</div>
-}
+})
