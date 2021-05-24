ec917784bdd38948727312ae4fc013a1af638666
ait <ait@ex.in>
2021-05-24 15:23:46 +0900

    Add Example TypeScript Basic

---
+++ src/typeTest.ts
@@ -0,0 +1,23 @@
+let bool: boolean = true
+
+let n: number = 12
+
+let str: string = "AAA"
+
+let arr1: Array<number> = [0, 1, 2]
+let arr2: number[] = [0, 1, 2]
+
+let tuple: [number, string] = [0, "AAA"]
+
+const funcA = (): void => {
+    const test = "Test"
+}
+
+let null1: null = null
+
+let undefined1: undefined = undefined
+
+let obj: object = {}
+let obj2: {
+    id: number, name: string
+} = { id: 2, name: "CCC" }
