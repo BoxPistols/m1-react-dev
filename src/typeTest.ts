61419aeed7bf55a4f897f6804d28dc33bbeecb37
ait <ait@ex.in>
2021-05-25 12:57:15 +0900

    clear code

--- src/typeTest.ts
+++ src/typeTest.ts
@@ -1,23 +1,28 @@
-let bool: boolean = true
+{
+    let bool: boolean = true
 
-let n: number = 12
+    let n: number = 12
 
-let str: string = "AAA"
+    let str: string = "AAA"
 
-let arr1: Array<number> = [0, 1, 2]
-let arr2: number[] = [0, 1, 2]
+    let arr1: Array<number> = [0, 1, 2]
+    let arr2: number[] = [0, 1, 2]
 
-let tuple: [number, string] = [0, "AAA"]
+    let tuple: [number, string] = [0, "AAA"]
 
-const funcA = (): void => {
-    const test = "Test"
+    const funcA = (): void => {
+        const test = "Test"
+    }
+
+    let null1: null = null
+
+    let undefined1: undefined = undefined
+
+    let obj: object = {}
+    let obj2: {
+        id: number, name: string
+    } = { id: 2, name: "CCC" }
 }
+{
 
-let null1: null = null
-
-let undefined1: undefined = undefined
-
-let obj: object = {}
-let obj2: {
-    id: number, name: string
-} = { id: 2, name: "CCC" }
+}
