5979deb6120f4bfd24a37e23ecc71fe99fd563e2
ait <ait@ex.in>
2021-07-11 06:04:09 +0900

    Add PropsType / Add Polished for Sass Custom

---
+++ src/pages/PropTypePage.jsx
@@ -0,0 +1,62 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
+import styled from '@emotion/styled'
+import PropTypes from 'prop-types'
+import { lighten, between, backgroundImages } from 'polished'
+
+export const PropTypePage = () => {
+    const profiles = [
+        { name: 'Taro', age: 10 },
+        { name: 'Jiro', age: 12 },
+        { name: 'Saburo', age: 18 },
+    ]
+
+    return (
+        <Wrap>
+            <EmStyled>
+                <h3>PropTypePage</h3>
+                <EmUl>
+                    {profiles.map((profile, index) => {
+                        return (
+                            // <li key={index}>
+                            //     {profile.name} : {profile.age}
+                            // </li>
+                            <User name={profile.name} age={profile.age} key={index} />
+                        )
+                    })}
+                </EmUl>
+                <p>{}</p>
+            </EmStyled>
+        </Wrap>
+    )
+}
+
+const User = (props) => {
+    return (
+        <li>
+            Hi, i,m {props.name} , and {props.age} years old
+        </li>
+    )
+}
+
+User.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired,
+}
+
+//  ===== Style =====
+const Wrap = styled.div`
+    display: flex;
+    justify-content: center;
+`
+const EmStyled = styled.div`
+    color: turquoise;
+    margin-top: 24px;
+    padding: 12px;
+`
+const EmUl = styled.ul`
+    font-size: ${between('14px', '20px', '300px', '800px')};
+    color: ${lighten(0.35, 'orange')};
+    ${backgroundImages('linear-gradient(#222,#999)')}
+`
--- src/router/BasicRouter.jsx
+++ src/router/BasicRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Rendering } from '../pages/Rendering'
 import { Styles } from '../pages/Styles'
 import { Page2 } from '../pages/Page2'
+import { PropTypePage } from '../pages/PropTypePage'
 
 export const BasicRouter = [
     {
@@ -18,5 +19,10 @@ export const BasicRouter = [
         path: 'page2',
         exact: true,
         children: <Page2 />,
+    },
+    {
+        path: 'PropTypePage',
+        exact: true,
+        children: <PropTypePage />,
     },
 ]
