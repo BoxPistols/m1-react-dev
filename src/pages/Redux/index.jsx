0bfad3762257f225f9614f037a05d8a39940ade8
ait <ait@ex.in>
2021-07-11 18:19:22 +0900

    Create Reducer Action

---
+++ src/pages/Redux/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+// import Reducers from '../../redux/reducers'
+export const Redux = () => {
+    return <div>Redux</div>
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux'
+import count from './counter'
+
+export default combineReducers({ count })
