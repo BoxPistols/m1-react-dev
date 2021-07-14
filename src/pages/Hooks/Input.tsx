ec4e6dab970f7436ea949ef38ca976c236f70b3c
ait <ait@ex.in>
2021-07-14 22:44:12 +0900

    Update Form use useState multi-Form

--- src/pages/Hooks/Input.tsx
+++ src/pages/Hooks/Input.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export const Input = () => {
-    const [name, setName] = useState('xxx')
+    const [name, setName] = useState({ first: 'bob', last: 'duran' })
 
     return (
         <div>
@@ -9,9 +9,12 @@ export const Input = () => {
                 <div className='column column-50 column-offset-25'>
                     <h4>Input</h4>
                     <div>
-                        <p>{name}</p>
+                        <p>
+                            {name.first} / {name.last}
+                        </p>
                         <form>
-                            <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                            <input type='text' value={name.first} onChange={(e) => setName((preName) => ({ ...preName, first: e.target.value }))} />
+                            <input type='text' value={name.last} onChange={(e) => setName((preName) => ({ ...preName, last: e.target.value }))} />
                         </form>
                     </div>
                 </div>
